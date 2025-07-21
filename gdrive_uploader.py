0b373b4e141330d2cdf1218100d026eeed4305ec
Huntingbots2 <tgh191119@gmail.com>
2025-07-21 17:27:35 +0530

    Update gdrive_uploader.py

--- gdrive_uploader.py
+++ gdrive_uploader.py
@@ -1,1 +1,38 @@
-# Google Drive uploader logic placeholder
+import os
+import mimetypes
+from google.oauth2.service_account import Credentials
+from googleapiclient.discovery import build
+from configparser import ConfigParser
+
+config = ConfigParser()
+config.read("config.ini")
+
+SCOPES = ['https://www.googleapis.com/auth/drive.file']
+SERVICE_ACCOUNT_FILE = config.get("GOOGLE", "Credentials")
+FOLDER_ID = config.get("GOOGLE", "FolderID")
+
+creds = Credentials.from_service_account_file(SERVICE_ACCOUNT_FILE, scopes=SCOPES)
+drive_service = build('drive', 'v3', credentials=creds)
+
+def upload_file(file_path: str, filename: str = None) -> str:
+    if not filename:
+        filename = os.path.basename(file_path)
+
+    file_metadata = {
+        'name': filename,
+        'parents': [FOLDER_ID]
+    }
+
+    mime_type, _ = mimetypes.guess_type(file_path)
+    media = None
+
+    from googleapiclient.http import MediaFileUpload
+    media = MediaFileUpload(file_path, mimetype=mime_type, resumable=True)
+
+    uploaded_file = drive_service.files().create(
+        body=file_metadata,
+        media_body=media,
+        fields='id, webViewLink'
+    ).execute()
+
+    return uploaded_file.get("webViewLink")
